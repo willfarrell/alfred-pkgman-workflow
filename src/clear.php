307c451f62c07f04b551c3c66fe2bfe8c96d09ab
Jeff Byrnes <jeff@evertrue.com>
2014-12-15 17:34:17 -0500

    Fix clear script to use namespaced classes

--- src/clear.php
+++ src/clear.php
@@ -2,11 +2,11 @@
 // ****************
 //error_reporting(0);
 require_once('Cache.php');
+require_once('Repo.php');
 
-$cache = new Cache();
+$cache = new WillFarrell\AlfredPkgMan\Cache();
 
 $cache->clear();
 
 //$cache->w->result( 'cachedb', 'NA', 'Downloading... ', 'You\'ll get a notification when it\'s done. It should be soon.', 'icon.png', 'no' );
 // ****************
-?>

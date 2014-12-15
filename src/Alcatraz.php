92cb6f4b58b27cbff6248fb0a8931c0325eafb73
Jeff Byrnes <jeff@evertrue.com>
2014-12-15 17:22:44 -0500

    Refactor Alcatraz to use new Repo class
    Has the side-effect of actually fixing this search, as this search
    did not, in fact, really properly work, returning odd results,
    owing the the multi-dimensional nature of the result object.
    
    Rename to use capitalization, as per PSR naming conventions

--- src/Alcatraz.php
+++ src/Alcatraz.php
@@ -1,119 +1,51 @@
 <?php
 namespace WillFarrell\AlfredPkgMan;
 
-/*
-Alcatraz
-
-*/
-
-// ****************
-
 require_once('Cache.php');
+require_once('Repo.php');
 
-class Repo {
-	
-	private $id = 'alcatraz';
-	private $kind = 'packages'; // for none found msg
-	private $min_query_length = 1; // increase for slow DBs
-	private $max_return = 25;
-	
-	private $cache;
-	private $w;
-	private $pkgs;
-	
-	function __construct() {
-		
-		$this->cache = new Cache();
-		
-		// get DB here if not dynamic search
-		$data = (array) $this->cache->get_db($this->id)->packages;
-		$this->pkgs = $data;
-	}
-	
-	// return id | url | pkgstr
-	function makeArg($id, $url, $version) {
-		return $id . "|" . $url . "|" . $id;//"\"$id\":\"$version\",";
-	}
-	
-	function check($pkg, $query) {
-		if (!$query) { return true; }
-		if (strpos($pkg->name, $query) !== false) {
-			return true;
-		} else if (strpos($pkg->description, $query) !== false) {
-			return true;
-		}
-		return false;
-	}
-	
-	function search($query) {
-		if ( strlen($query) < $this->min_query_length ) {
-			if ( strlen($query) === 0 ) { return; }
-			$this->cache->w->result(
-				"{$this->id}-min",
-				$query,
-				"Minimum query length of {$this->min_query_length} not met.",
-				"",
-				"icon-cache/{$this->id}.png"
-			);
-			return;
+class Alcatraz extends Repo
+{
+	protected $id     = 'alcatraz';
+	protected $url    = 'http://alcatraz.io';
+	protected $has_db = true;
+
+	public function search($query)
+	{
+		if (!$this->hasMinQueryLength($query)) {
+			return $this->xml(); 
 		}
 		
-		foreach($this->pkgs as $pkg ) {
+		foreach($this->pkgs as $categories) {
 			// plugins, color_scheme, project_templates, file_templates
-			for( $i = 0; $i < count($pkg); $i++ ) {
-				
-				if ($this->check($pkg[$i], $query)) {
-					$this->cache->w->result(
-						$pkg[$i]->url,
-						$this->makeArg($pkg[$i]->name, $pkg[$i]->url, "*"),
-						$pkg[$i]->name,
-						$pkg[$i]->description,
-						"icon-cache/{$this->id}.png"
-					);
-				}
-				
-				// only search till max return reached
-				if ( count ( $this->cache->w->results() ) == $this->max_return ) {
-					break;
+			foreach($categories as $category) {
+				foreach($category as $pkg) {
+					if ($this->check($pkg, $query)) {
+						$this->cache->w->result(
+							$pkg->url,
+							$this->makeArg($pkg->name, $pkg->url),
+							$pkg->name,
+							$pkg->description,
+							"icon-cache/{$this->id}.png"
+						);
+					}
+
+					// only search till max return reached
+					if ( count ( $this->cache->w->results() ) == $this->max_return ) {
+						break;
+					}
 				}
 			}
 		}
 		
-		if ( count( $this->cache->w->results() ) == 0) {
-			$this->cache->w->result(
-				"{$this->id}-search",
-				"http://alcatraz.io//{$query}", // UPDATE NEEDED
-				"No {$this->kind} were found that matched \"{$query}\"",
-				"Click to see the results for yourself",
-				"icon-cache/{$this->id}.png"
-			);
-		}
-	}
-	
-	function xml() {
-		
-		
-		
-		$this->cache->w->result(
-			$this->id.'-www',
-			'http://alcatraz.io//',
-			'Go to the website',
-			'http://alcatraz.io/',
-			"icon-cache/".$this->id.".png"
-		);
-		
-		return $this->cache->w->toxml();
-	}
+		// The search URL here isn’t quiet right, since Alcatraz doesn't have a
+		// a web UI for its "repo"
+		$this->noResults($query, $this->cache->dbs[$this->id]);
 
+		return $this->xml();
+	}
 }
 
-// ****************
-
-/*
-$query = "a";
-$repo = new Repo();
-$repo->search($query);
-echo $repo->xml();
-*/
-
-?>
+// Test code, uncomment to debug this script from the command-line
+// $repo = new Alcatraz();
+// echo $repo->search('avia');

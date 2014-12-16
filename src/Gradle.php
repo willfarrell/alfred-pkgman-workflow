f268d624d833d1f617cca1b4ed4defb8e26fbef0
Jeff Byrnes <jeff@evertrue.com>
2014-12-16 10:45:07 -0500

    Update gradle & maven to use new Repo class
    As these two classes are identical to each other, save their names,
    I'm not sure why both exist.
    
    Some discrepancies between them have been recitifed, and they now
    consistently both search & link to search.maven.org
    
    This is in opposition to the previous behavior of searching
    search.maven.org from w/i the workflow, yet linking to the search
    results page from mvnrepository.com

--- src/Gradle.php
+++ src/Gradle.php
@@ -1,76 +1,38 @@
 <?php
 namespace WillFarrell\AlfredPkgMan;
 
-/*
-Java gradle
-
-*/
-
-// ****************
-
 require_once('Cache.php');
+require_once('Repo.php');
 
-class Repo {
-	
-	private $id = 'gradle';
-	private $kind = 'packages'; // for none found msg
-	private $min_query_length = 1; // increase for slow DBs
-	private $max_return = 25;
-	
-	private $cache;
-	private $w;
-	private $pkgs;
-	
-	function __construct() {
-		
-		$this->cache = new Cache();
-		
-		// get DB here if not dynamic search
-		//$data = (array) $this->cache->get_db($this->id);
-		//$this->pkgs = $data;
-	}
-	
-	// return id | url | pkgstr
-	function makeArg($id, $url, $version) {
-		return $id . "|" . $url . "|" . $id;//"\"$id\":\"$version\",";
-	}
-	
-	/*function check($pkg, $query) {
-		if (!$query) { return true; }
-		if (strpos($pkg["name"], $query) !== false) {
-			return true;
-		} else if (strpos($pkg["description"], $query) !== false) {
-			return true;
-		} 
-	
-		return false;
-	}*/
-	
-	function search($query) {
-		if ( strlen($query) < $this->min_query_length) {
-			if ( strlen($query) === 0 ) { return; }
-			$this->cache->w->result(
-				"{$this->id}-min",
-				$query,
-				"Minimum query length of {$this->min_query_length} not met.",
-				"",
-				"icon-cache/{$this->id}.png"
-			);
-			return;
+class Gradle extends Repo
+{
+	protected $id         = 'gradle';
+	protected $kind       = 'libraries';
+	protected $url        = 'http://search.maven.org';
+	protected $search_url = 'http://search.maven.org/#search|ga|1|';
+
+	public function search($query)
+	{
+		if (!$this->hasMinQueryLength($query)) {
+			return $this->xml(); 
 		}
 		
-		$this->pkgs = $this->cache->get_query_json($this->id, $query, 'http://search.maven.org/solrsearch/select?q='.$query.'&rows=10&wt=json')->response->docs;
+		$this->pkgs = $this->cache->get_query_json(
+			$this->id,
+			$query,
+			"{$this->url}/solrsearch/select?q={$query}&rows=10&wt=json"
+		)->response->docs;
 		
 		foreach($this->pkgs as $pkg) {
 			
 			// make params
-			$title = $pkg->a.' ('.$pkg->latestVersion.')';
-			$url = 'http://search.maven.org/#artifactdetails%7C'.$pkg->g.'%7C'.$pkg->a.'%7C'.$pkg->latestVersion.'%7C'.$pkg->p;
-			$details = 'GroupId: '.$pkg->id;
+			$title = "{$pkg->a} (v{$pkg->latestVersion})";
+			$url = "{$this->url}/#artifactdetails|{$pkg->g}|{$pkg->a}|{$pkg->latestVersion}|{$pkg->p}";
+			$details = "GroupId: {$pkg->id}";
 	
 			$this->cache->w->result(
 				$pkg->a,
-				$this->makeArg($pkg->a, $url, "*"),
+				$this->makeArg($pkg->a, $url),
 				$title,
 				$details,
 				"icon-cache/{$this->id}.png"
@@ -81,39 +43,13 @@ class Repo {
 				break;
 			}
 		}
-		
-		if ( count( $this->cache->w->results() ) == 0) {
-			$this->cache->w->result(
-				"{$this->id}-search",
-				"http://mvnrepository.com/search.html?query={$query}",
-				"No {$this->kind} were found that matched \"{$query}\"",
-				"Click to see the results for yourself",
-				"icon-cache/{$this->id}.png"
-			);
-		}
-	}
-	
-	function xml() {
-		$this->cache->w->result(
-			"{$this->id}-www",
-			'http://www.gradle.org/',
-			'Go to the website',
-			'http://www.gradle.org',
-			"icon-cache/{$this->id}.png"
-		);
-		
-		return $this->cache->w->toxml();
-	}
 
+		$this->noResults($query, "{$this->search_url}{$query}");
+
+		return $this->xml();
+	}
 }
 
-// ****************
-
-/*
-$query = "leaflet";
-$repo = new Repo();
-$repo->search($query);
-echo $repo->xml();
-*/
-
-?>
+// Test code, uncomment to debug this script from the command-line
+// $repo = new Gradle();
+// echo $repo->search('leaflet');

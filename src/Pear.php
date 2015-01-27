f11f6b083277a6fa560f706d2f551d41d790398c
Jeff Byrnes <jeff@evertrue.com>
2015-01-27 10:39:47 -0500

    Refactor pear to use new Repo class

--- src/Pear.php
+++ src/Pear.php
@@ -1,65 +1,28 @@
 <?php
 namespace WillFarrell\AlfredPkgMan;
 
-/*
-pear
-
-*/
-
-// ****************
-
 require_once('Cache.php');
+require_once('Repo.php');
 
-class Repo {
-	
-	private $id = 'pear';
-	private $kind = 'packages'; // for none found msg
-	private $min_query_length = 1; // increase for slow DBs
-	private $max_return = 25;
-	
-	private $cache;
-	private $w;
-	private $pkgs;
-	
-	function __construct() {
-		
-		$this->cache = new Cache();
-		
-		// get DB here if not dynamic search
-		//$data = (array) $this->cache->get_db($this->id);
-		//$this->pkgs = $data;
-	}
-	
-	// return id | url | pkgstr
-	function makeArg($id, $url, $version) {
-		return $id . "|" . $url . "|" . $id;//"\"$id\":\"$version\",";
-	}
-	
-	/*function check($pkg, $query) {
-		if (!$query) { return true; }
-		if (strpos($pkg["name"], $query) !== false) {
-			return true;
-		} else if (strpos($pkg["description"], $query) !== false) {
-			return true;
-		} 
-	
-		return false;
-	}*/
-	
-	function search($query) {
-		if ( strlen($query) < $this->min_query_length) {
-			if ( strlen($query) === 0 ) { return; }
-			$this->cache->w->result(
-				"{$this->id}-min",
-				$query,
-				"Minimum query length of {$this->min_query_length} not met.",
-				"",
-				"icon-cache/{$this->id}.png"
-			);
-			return;
+class Pear extends Repo
+{
+	protected $id         = 'pear';
+	protected $url        = 'http://pear.php.net';
+	protected $search_url = 'http://pear.php.net/search.php?q=';
+
+	public function search($query)
+	{
+		if (!$this->hasMinQueryLength($query)) {
+			return $this->xml(); 
 		}
 		
-		$this->pkgs = $this->cache->get_query_regex($this->id, $query, 'http://pear.php.net/search.php?q='.$query, '/<li>([\s\S]*?)<\/li>/i');
+		$this->pkgs = $this->cache->get_query_regex(
+			$this->id,
+			$query,
+			"{$this->search_url}{$query}",
+			'/<li>([\s\S]*?)<\/li>/i'
+		);
+		
 		array_shift($this->pkgs); // remove register link
 		
 		foreach($this->pkgs as $pkg) {
@@ -74,7 +37,7 @@ class Repo {
 	
 			$this->cache->w->result(
 				$title,
-				$this->makeArg($title, 'http://pear.php.net/package/'.$title, "*"),
+				$this->makeArg($title, "{$this->url}/package/{$title}"),
 				$title,
 				$details,
 				"icon-cache/{$this->id}.png"
@@ -85,39 +48,13 @@ class Repo {
 				break;
 			}
 		}
-		
-		if ( count( $this->cache->w->results() ) == 0) {
-			$this->cache->w->result(
-				"{$this->id}-search",
-				"http://pear.php.net/search.php?q={$query}",
-				"No {$this->kind} were found that matched \"{$query}\"",
-				"Click to see the results for yourself",
-				"icon-cache/{$this->id}.png"
-			);
-		}
-	}
-	
-	function xml() {
-		$this->cache->w->result(
-			"{$this->id}-www",
-			'http://pear.php.net/',
-			'Go to the website',
-			'http://pear.php.net',
-			"icon-cache/{$this->id}.png"
-		);
-		
-		return $this->cache->w->toxml();
-	}
 
+		$this->noResults($query, "{$this->search_url}{$query}");
+
+		return $this->xml();
+	}
 }
 
-// ****************
-
-/*
-$query = "l";
-$repo = new Repo();
-$repo->search($query);
-echo $repo->xml();
-*/
-
-?>
+// Test code, uncomment to debug this script from the command-line
+// $repo = new Pear();
+// echo $repo->search('test');

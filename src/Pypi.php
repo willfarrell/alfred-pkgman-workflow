3303d555a1e8240ca5dc0e42752bd439fd663ddb
Jeff Byrnes <jeff@evertrue.com>
2015-01-28 12:50:09 -0500

    Refactor Pypi to use new Repo class

--- src/Pypi.php
+++ src/Pypi.php
@@ -1,65 +1,29 @@
 <?php
 namespace WillFarrell\AlfredPkgMan;
 
-/*
-Python pypi
-
-*/
-
-// ****************
-
 require_once('Cache.php');
+require_once('Repo.php');
 
-class Repo {
-	
-	private $id = 'pypi';
-	private $kind = 'packages'; // for none found msg
-	private $min_query_length = 3; // increase for slow DBs
-	private $max_return = 25;
-	
-	private $cache;
-	private $w;
-	private $pkgs;
-	
-	function __construct() {
-		
-		$this->cache = new Cache();
-		
-		// get DB here if not dynamic search
-		//$data = (array) $this->cache->get_db($this->id);
-		//$this->pkgs = $data;
-	}
-	
-	// return id | url | pkgstr
-	function makeArg($id, $url, $version) {
-		return $id . "|" . $url . "|" . $id;//"\"$id\":\"$version\",";
-	}
-	
-	/*function check($pkg, $query) {
-		if (!$query) { return true; }
-		if (strpos($pkg["name"], $query) !== false) {
-			return true;
-		} else if (strpos($pkg["description"], $query) !== false) {
-			return true;
-		} 
-	
-		return false;
-	}*/
-	
-	function search($query) {
-		if ( strlen($query) < $this->min_query_length) {
-			if ( strlen($query) === 0 ) { return; }
-			$this->cache->w->result(
-				"{$this->id}-min",
-				$query,
-				"Minimum query length of {$this->min_query_length} not met.",
-				"",
-				"icon-cache/{$this->id}.png"
-			);
-			return;
+class Pypi extends Repo
+{
+	protected $id               = 'pypi';
+	protected $url              = 'https://pypi.python.org';
+	protected $search_url       = 'https://pypi.python.org/pypi?%3Aaction=search&submit=search&term=';
+	protected $min_query_length = 3;
+
+	public function search($query)
+	{
+		if (!$this->hasMinQueryLength($query)) {
+			return $this->xml(); 
 		}
-		
-		$this->pkgs = $this->cache->get_query_regex($this->id, $query, 'https://pypi.python.org/pypi?%3Aaction=search&term='.$query.'&submit=search', '/<tr class="(.*?)">([\s\S]*?)<\/tr>/i', 2);
+
+		$this->pkgs = $this->cache->get_query_regex(
+			$this->id,
+			$query,
+			"{$this->search_url}{$query}",
+			'/<tr class="(.*?)">([\s\S]*?)<\/tr>/i',
+			2
+		);
 		
 		foreach($this->pkgs as $pkg) {
 			
@@ -75,8 +39,8 @@ class Repo {
 		
 			$this->cache->w->result(
 				$title,
-				$this->makeArg($title, 'https://pypi.python.org'.$url, "*"),
-				$title."    ".$downloads,
+				$this->makeArg($title, "{$this->url}{$url}"),
+				"{$title}    {$downloads}",
 				$details,
 				"icon-cache/{$this->id}.png"
 			);
@@ -86,39 +50,12 @@ class Repo {
 				break;
 			}
 		}
-		
-		if ( count( $this->cache->w->results() ) == 0) {
-			$this->cache->w->result(
-				"{$this->id}-search",
-				"https://pypi.python.org/pypi?%3Aaction=search&term={$query}&submit=search",
-				"No {$this->kind} were found that matched \"{$query}\"",
-				"Click to see the results for yourself",
-				"icon-cache/{$this->id}.png"
-			);
-		}
-	}
-	
-	function xml() {
-		$this->cache->w->result(
-			"{$this->id}-www",
-			'https://pypi.python.org/',
-			'Go to the website',
-			'https://pypi.python.org',
-			"icon-cache/{$this->id}.png"
-		);
-		
-		return $this->cache->w->toxml();
-	}
 
+		$this->noResults($query, "{$this->search_url}{$query}");
+
+		return $this->xml();
+	}
 }
 
-// ****************
-
-/*
-$query = "lib";
-$repo = new Repo();
-$repo->search($query);
-echo $repo->xml();
-*/
-
-?>
+// $repo = new Pypi();
+// echo $repo->search('lib');

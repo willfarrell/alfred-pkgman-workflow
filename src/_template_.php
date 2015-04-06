f6fa42efd308e49b5af2b0d9a442a6918309a872
will Farrell <will.farrell@gmail.com>
2015-04-06 12:29:13 -0600

    Template extended to include additional optinons

--- src/_template_.php
+++ src/_template_.php
@@ -20,16 +20,21 @@ require_once('Repo.php');
  */
 class RepoName extends Repo
 {
-	protected $id         = '_TEMPLATE_';
-	protected $url        = '_BASE_URL_';
-	protected $search_url = '_SEARCH_URL_';
-
+	protected $id         		= '_TEMPLATE_';
+	protected $url        		= '_BASE_URL_';
+	protected $search_url 		= '_SEARCH_URL_';
+	
+	// Optional
+	//protected $min_query_length = 1;
+	//protected $has_db     		= false; // For repos with small static DB sets. Update in Cache.php required.
+	
 	public function search($query)
 	{
 		if (!$this->hasMinQueryLength($query)) {
 			return $this->xml(); 
 		}
-
+		
+		// for repos without an API, try $this->cache->get_query_regex()
 		$this->pkgs = $this->cache->get_query_json(
 			$this->id,
 			$query,
@@ -41,7 +46,7 @@ class RepoName extends Repo
 			
 			$this->cache->w->result(
 				$this->id,
-				$this->makeArg(_PKG_NAME_, _PKG_URL_),
+				$this->makeArg(_PKG_NAME_, _PKG_URL_, _PKG_CONF_STR_FORMAT_),
 				_PKG_NAME_,
 				_PKG_DETAILS_OR_URL_,
 				"icon-cache/{$this->id}.png"

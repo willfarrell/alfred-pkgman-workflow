2ce9cee1d128b18489b7d44b653fa632007e92f6
Jeff Byrnes <thejeffbyrnes@gmail.com>
2017-01-04 13:29:25 -0500

    Clean up whitespace in Npm
    Spaces & tabs don’t mix, use EditorConfig to align w/ the project

--- src/Npm.php
+++ src/Npm.php
@@ -9,21 +9,20 @@ class Npm extends Repo
 	protected $id         = 'npm';
 	protected $url        = 'https://npms.io';
 	protected $search_url = 'https://api.npms.io/v2/search?q=';
-  
-  public function search($query)
-  {
-    if (!$this->hasMinQueryLength($query)) {
+
+	public function search($query)
+	{
+		if (!$this->hasMinQueryLength($query)) {
 			return $this->xml(); 
 		}
-    
-    $this->pkgs = $this->cache->get_query_json(
+
+		$this->pkgs = $this->cache->get_query_json(
 			$this->id,
 			$query,
 			"{$this->search_url}{$query}&size={$this->max_return}"
 		);
-    
-    foreach($this->pkgs->results as $pkg) {
-      $p = $pkg->package;
+		foreach($this->pkgs->results as $pkg) {
+			$p = $pkg->package;
 			$name = $p->name;
 			
 			$this->cache->w->result(
@@ -43,7 +42,7 @@ class Npm extends Repo
 		$this->noResults($query, "{$this->search_url}{$query}");
 
 		return $this->xml();
-  }
+	}
 }
 
 // Test code, uncomment to debug this script from the command-line

37aaaa7c3a7e7afca0fcc83db6b33f1ad971b34f
Josselin Auguste <jauguste@iblop.net>
2018-04-16 13:46:26 +0200

    Add hackage

---
+++ src/Hackage.php
@@ -0,0 +1,50 @@
+<?php
+namespace WillFarrell\AlfredPkgMan;
+
+require_once('Repo.php');
+
+class Hackage extends Repo
+{
+    protected $id               = 'hackage';
+    protected $url              = 'https://hackage.haskell.org';
+    protected $search_url       = 'https://hackage.haskell.org/packages/search.json?terms=';
+    protected $package_url      = 'https://hackage.haskell.org/package/';
+
+    public function search($query)
+    {
+        if (!$this->hasMinQueryLength($query)) {
+            return $this->xml();
+        }
+
+        $this->pkgs = $this->cache->get_query_json(
+            $this->id,
+            $query,
+            "{$this->search_url}{$query}"
+        );
+
+        foreach ($this->pkgs as $pkg) {
+            $name = $pkg->name;
+            $url = "{$package_url}$pkg->name";
+            $this->cache->w->result(
+                $name,
+                $this->makeArg($name, $url, "- {$name}"),
+                $name,
+                "",
+                "icon-cache/{$this->id}.png"
+            );
+
+            // only search till max return reached
+            if (count($this->cache->w->results()) == $this->max_return) {
+                break;
+            }
+        }
+
+        $this->noResults($query, "{$this->search_url}{$query}");
+
+        return $this->xml();
+    }
+}
+
+// Test code, uncomment to debug this script from the command-line
+// $repo = new Hackage();
+// echo $repo->search('hspec');
